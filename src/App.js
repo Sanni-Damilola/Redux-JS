cce1eb15f948d27036a0f6e2bccca08a3979f590
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-08 12:33:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Login from "./Components/Login/Login";
+import Profile from "./Components/Profile/Profile";
+
+function App() {
+  return (
+    <center style={{ marginTop: "300px" }}>
+      <br />
+      <Login />
+      <Profile />
+    </center>
+  );
+}
+
+export default App;
