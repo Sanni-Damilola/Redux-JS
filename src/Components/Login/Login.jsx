3d4130a4b0dad4424493c80bede2be6c68a2ef90
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-10 13:38:08 +0100

    update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,25 +4,67 @@ import { loginUser, logOutUser } from "../ReduxState/ReduxState";
 
 const Login = () => {
   const dispatch = useDispatch(); // function useage 👇👇✔✔
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [Age, setAge] = React.useState("");
+  const [stack, setStack] = React.useState("");
 
   return (
     <div>
-      <button
-        onClick={() => {
-          dispatch(
-            // here 👇✔❤❕
-            loginUser({
-              userName: "favour",
-              Age: "10yrs",
-              Email: "favour@gmaul.com",
-              Stack: "FullStack",
-            })
-          ); //  store data
+      <input
+        type="email"
+        onChange={(e) => {
+          setEmail(e.target.value);
         }}
-        style={{ cursor: "pointer", padding: "10px 30px", margin: "10px" }}
-      >
-        Login
-      </button>
+        name=""
+        placeholder="email"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setStack(e.target.value);
+        }}
+        type="text"
+        name=""
+        placeholder="stack"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        name=""
+        placeholder="name"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setAge(e.target.value);
+        }}
+        type="text"
+        name=""
+        placeholder="age"
+      />{" "}
+      <br />
+      {name !== "" && Age !== "" && email !== "" && stack !== "" ? (
+        <button
+          onClick={() => {
+            dispatch(
+              // here 👇✔❤❕
+              loginUser({
+                userName: name,
+                Age: Age,
+                Email: email,
+                Stack: stack,
+              })
+            ); //  store data
+          }}
+          style={{ cursor: "pointer", padding: "10px 30px", margin: "10px" }}
+        >
+          Login
+        </button>
+      ) : null}
       <button
         onClick={() => {
           dispatch(logOutUser());
