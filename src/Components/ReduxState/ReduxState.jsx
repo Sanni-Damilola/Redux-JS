ed8179996f67b417c1ad74babc51f7f29d8d805d
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:11:07 +0100

    update

--- src/Components/ReduxState/ReduxState.jsx
+++ src/Components/ReduxState/ReduxState.jsx
@@ -3,3 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   current: null,
 };
+
+
+const ReducerState = createSlice({
+  name: "users",
+  initialState,
+  reducers: {
+    logIn: (state, action) => {
+
+    }
+  }
+})
