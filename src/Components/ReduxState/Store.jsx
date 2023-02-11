62f3bc7cf3148c008572b714b48c2770ff884656
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:50:27 +0100

    update

--- src/Components/ReduxState/Store.jsx
+++ src/Components/ReduxState/Store.jsx
@@ -1,8 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import myReducer from "./ReduxState";
+// import { configureStore } from "@reduxjs/toolkit";
+// import myReducer from "./ReduxState";
 
-export const store = configureStore({
-  reducer: {
-    myReducer,
-  },
-}); // storeing the reducer
+import { configureStore } from "@reduxjs/toolkit";
+
+// export const store = configureStore({
+//   reducer: {
+//     myReducer,
+//   },
+// }); // storeing the reducer
+
+
+export
