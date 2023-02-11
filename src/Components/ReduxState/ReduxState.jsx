3ac1a442cdc349ed5e726691ea4c1af8f04f63b4
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:16:58 +0100

    update

--- src/Components/ReduxState/ReduxState.jsx
+++ src/Components/ReduxState/ReduxState.jsx
@@ -4,9 +4,19 @@ const initialSTate = {
   current: null,
 };
 
-
-
 const reducerState = createSlice({
   name: "user",
   initialState,
-})
+  reducers: {
+    logIn: (state, action) => {
+      state.current = action.payload;
+    },
+    logOut: (state, action) => {
+      state.current = null;
+    },
+  },
+});
+
+export const { logIn, logOut } = reducerState.actions;
+
+export default reducerState.reducer;
