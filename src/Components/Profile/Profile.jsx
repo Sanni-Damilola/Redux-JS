4ec3190becb75126bf5c67b05e6049ed5857a32e
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 21:32:06 +0100

    update

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -6,10 +6,10 @@ const Profile = () => {
   return (
     <div>
       <h1>User</h1>
-      <div>userName: {userData.name}</div>
-      <div>emai</div>
-      <div></div>
-      <div></div>
+      <div>userName: {userData?.name}</div>
+      <div>email : {userData?.email}</div>
+      <div>age: {userData?.age}</div>
+      <div>stack : {userData.stack}</div>
     </div>
   );
 };
