8fd7a81bad282a7fd7c0a76df6c5617ef144a5e7
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:53:33 +0100

    update

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
