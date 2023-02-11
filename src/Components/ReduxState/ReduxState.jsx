87be943ea7959d2ac2e9ca596ef913c7b2aa4aa4
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:28:01 +0100

    update

--- src/Components/ReduxState/ReduxState.jsx
+++ src/Components/ReduxState/ReduxState.jsx
@@ -1,22 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialSTate = {
+const initialState = {
   current: null,
 };
 
-const reducerState = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    logIn: (state, action) => {
-      state.current = action.payload;
-    },
-    logOut: (state, action) => {
-      state.current = null;
-    },
-  },
-});
 
-export const { logIn, logOut } = reducerState.actions;
-
-export default reducerState.reducer;
