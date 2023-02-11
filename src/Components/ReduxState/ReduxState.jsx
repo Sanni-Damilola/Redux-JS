cff90c54046344ef13ab706a21ea698b4079118d
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:14:25 +0100

    update

--- src/Components/ReduxState/ReduxState.jsx
+++ src/Components/ReduxState/ReduxState.jsx
@@ -1,22 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+const initialSTate = {
   current: null,
 };
 
-const ReducerState = createSlice({
-  name: "users",
-  initialState,
-  reducers: {
-    logIn: (state, action) => {
-      state.current = action.payload;
-    },
-    logOut: (state, action) => {
-      state.current = null;
-    },
-  },
-});
 
-export const { logIn, logOut } = ReducerState.actions;
 
-export default ReducerState.reducer;
+const reducerState = createSlice({
+  sta
+})
