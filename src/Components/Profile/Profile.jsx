4c37b659ff51b1047edbfee81b750e4723134f30
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:55:59 +0100

    update

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { logInUser, logOutUser } from "../ReduxState/ReduxState";
 
 const Profile = () => {
-
-  
-
   return <div>Profile</div>;
 };
 
