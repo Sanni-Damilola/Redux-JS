8bb7481f8944257c10f9423f0515092bb33e0ac7
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 20:53:12 +0100

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { store } from "./Components/ReduxState/Store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>
 );
