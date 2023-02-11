018d136ab4a89541228c998c0b6351abc4480027
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 21:31:01 +0100

    update

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 const Profile = () => {
- 
+  const userData = React.useSeletor((state) => state.myReducer.current);
 
   return (
     <div>
-     <h1>User</h1>
+      <h1>User</h1>
+      <div>userName: {userData.name}</div>
+      <div>emai</div>
+      <div></div>
+      <div></div>
     </div>
   );
 };
