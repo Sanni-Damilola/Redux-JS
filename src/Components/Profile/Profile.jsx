ecb441c9b3b3fbda593613db7d78fe3f39afcb8f
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 21:11:41 +0100

    update

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { logInUser } from "../ReduxState/ReduxState";
 
 const Profile = () => {
   const [name, setName] = React.useState("");
@@ -6,13 +7,31 @@ const Profile = () => {
   const [stack, setStack] = React.useState("");
   const [email, setEmail] = React.useState("");
 
+  const dispatch = React.useDispatch();
+
   return (
     <div>
-      <input type="email" placeholder="email" /> <br />
+      <input
+      
+      type="email" placeholder="email" /> <br />
       <input type="text" placeholder="name" /> <br />
       <input type="text" placeholder="stack" /> <br />
       <input type="number" placeholder="age" /> <br />
-      <button style={{ margin: "10px", padding: "5px" }}>login</button>
+      <button
+        onClick={() => {
+          dispatch(
+            logInUser({
+              name: name,
+              email: email,
+              stack: stack,
+              age: age,
+            })
+          );
+        }}
+        style={{ margin: "10px", padding: "5px" }}
+      >
+        login
+      </button>
       <button style={{ margin: "10px", padding: "5px" }}>logout</button>
     </div>
   );
