748f6b77e68032f88f60238377902e0dbafed7b0
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 21:16:20 +0100

    update

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { logInUser } from "../ReduxState/ReduxState";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [name, setName] = React.useState("");
+  const [age, setAge] = React.useState("");
+  const [stack, setStack] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
-export default Login
+  const dispatch = React.useDispatch(); // pushing data to the store
+
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        type="email"
+        placeholder="email"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        placeholder="name"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setStack(e.target.value);
+        }}
+        type="text"
+        placeholder="stack"
+      />{" "}
+      <br />
+      <input
+        onChange={(e) => {
+          setAge(e.target.value);
+        }}
+        type="number"
+        placeholder="age"
+      />{" "}
+      <br />
+      <button
+        onClick={() => {
+          dispatch(
+            logInUser({
+              name: name,
+              email: email,
+              stack: stack,
+              age: age,
+            })
+          );
+        }}
+        style={{ margin: "10px", padding: "5px" }}
+      >
+        login
+      </button>
+      <button style={{ margin: "10px", padding: "5px" }}>logout</button>
+    </div>
+  );
+};
+
+export default Login;
--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,64 +1,11 @@
 import React from "react";
-import { logInUser } from "../ReduxState/ReduxState";
 
 const Profile = () => {
-  const [name, setName] = React.useState("");
-  const [age, setAge] = React.useState("");
-  const [stack, setStack] = React.useState("");
-  const [email, setEmail] = React.useState("");
-
-  const dispatch = React.useDispatch();
+ 
 
   return (
     <div>
-      <input
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        type="email"
-        placeholder="email"
-      />{" "}
-      <br />
-      <input
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-        type="text"
-        placeholder="name"
-      />{" "}
-      <br />
-      <input
-        onChange={(e) => {
-          setStack(e.target.value);
-        }}
-        type="text"
-        placeholder="stack"
-      />{" "}
-      <br />
-      <input
-        onChange={(e) => {
-          setAge(e.target.value);
-        }}
-        type="number"
-        placeholder="age"
-      />{" "}
-      <br />
-      <button
-        onClick={() => {
-          dispatch(
-            logInUser({
-              name: name,
-              email: email,
-              stack: stack,
-              age: age,
-            })
-          );
-        }}
-        style={{ margin: "10px", padding: "5px" }}
-      >
-        login
-      </button>
-      <button style={{ margin: "10px", padding: "5px" }}>logout</button>
+     
     </div>
   );
 };
