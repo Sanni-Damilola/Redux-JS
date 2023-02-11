97325dff2c71cff4979fb655719312fba3bd687e
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-11 21:04:48 +0100

    update

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -3,11 +3,31 @@ import { useDispatch } from "react-redux";
 import { logInUser, logOutUser } from "../ReduxState/ReduxState";
 
 const Profile = () => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [stack, setStack] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-      <button style={{ margin: "10px", padding: "5px" }}>login</button>
-      <button style={{ margin: "10px", padding: "5px" }}>logout</button>
+      <button
+        onClick={() => {
+          dispatch(
+            logInUser({
+              name: name,
+              email: email,
+              stack: stack,
+              age: age,
+            })
+          );
+        }}
+        style={{ margin: "10px", padding: "5px" }}
+      >
+        login
+      </button>
+      <button
+      style={{ margin: "10px", padding: "5px" }}>logout</button>
     </div>
   );
 };
